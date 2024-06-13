93a3d77df0c9977bdbd8a84b2c6cfeec4d81c7d7
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-06-13 17:53:40 +0600

    versel setup done

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import router from "./app/routes";
 import notFound from "./app/middlewares/notFound";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
@@ -17,7 +17,10 @@ app.use(cors());
 
 // application routes
 app.use("/api", router);
-
+//home route to check connectivity
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 app.use(globalErrorHandler);
 
 //Not Found
